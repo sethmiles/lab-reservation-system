439477f00b3713ba49b3edc3ff362ce990e4f34c
Lane Sawyer <me@lanesawyer.com>
2014-01-20 15:38:58 -0700

    add config files

---
+++ config/env/development.js
@@ -0,0 +1,10 @@
+module.exports = {
+    db: {
+        name: "lab_reservation",
+        username: "root",
+        password: "password1",
+    },
+    app: {
+        name: "M*EAN Stack - Development"
+    },
+}
---
+++ config/sequelize.js
@@ -0,0 +1,38 @@
+var fs        = require('fs'),
+    path      = require('path'),
+    Sequelize = require('sequelize-mysql').sequelize,
+    lodash    = require('lodash'),
+    config    = require('./config'),
+    db        = {};
+
+console.log("Initializing Sequelize");
+
+var sequelize = new Sequelize(config.db.name, config.db.username, config.db.password);
+
+fs
+  .readdirSync(config.modelsDir)
+  .filter(function(file) {
+    return (file.indexOf('.') !== 0) && (file !== 'index.js');
+  })
+  .forEach(function(file) {
+    var model = sequelize.import(path.join(config.modelsDir, file));
+    db[model.name] = model;
+  });
+ 
+Object.keys(db).forEach(function(modelName) {
+  if (db[modelName].options.hasOwnProperty('associate')) {
+    db[modelName].options.associate(db);
+  }
+});
+ 
+sequelize
+  .sync({ force: true })
+  .complete(function(err) {
+    if (err) throw err;
+    else console.log("Database dropped and synchronized");
+  });
+
+module.exports = lodash.extend({
+  sequelize: sequelize,
+  Sequelize: Sequelize
+}, db);
