07704f8c2424befcd889a88f23db24a22e6b9dc3
Lane Sawyer <me@lanesawyer.com>
2014-02-07 14:10:04 -0700

    everything merged together

--- public/js/controllers/lrs.js
+++
@@ -1,7 +0,0 @@
-angular.module('lrs.system').controller('LRSController', ['$scope', 'Global', function ($scope, Global) {
-  $scope.global = Global;
-
-  $scope.test = function() {
-    alert('I work.');
-  };
-}]);
