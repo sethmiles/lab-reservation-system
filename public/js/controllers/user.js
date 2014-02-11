6ed5c35132bc60a32043fc9449968adccbaa7691
Lane Sawyer <me@lanesawyer.com>
2014-02-10 23:29:40 -0700

    add email modal

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -1,4 +1,4 @@
-angular.module('lrs').controller('UserController', ['$scope', '$http', 'Global', function ($scope, $http, Global) {
+angular.module('lrs').controller('UserController', ['$scope', '$http', '$modal', 'Global', function ($scope, $http, $modal, Global) {
   $scope.global = Global;
 
   $scope.login = function() {
@@ -14,5 +14,24 @@ angular.module('lrs').controller('UserController', ['$scope', '$http', 'Global',
     $http.post('logout').success(function(data, status, headers, config) {
       window.location.reload()
     });
+  };
+
+  $scope.getEmail = function() {
+    var modalInstance = $modal.open({
+      templateUrl: 'views/partials/addEmail.html',
+      controller: EmailController,
+      backdrop: 'static',
+      keyboard: false
+    });
+  };
+
+  var EmailController = function ($scope, $modalInstance) {
+    $scope.addEmail = function(email) {
+      $http.put('api/Users/' + Global.user.id, {
+        email: email
+      }).success(function(data, status, headers, config) {
+        $modalInstance.close();
+      });
+    };
   };
 }]);
