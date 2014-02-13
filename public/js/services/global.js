0c6386e38690c9ad9cb28319d9b24a5425baac4a
Lane Sawyer <me@lanesawyer.com>
2014-02-13 14:55:52 -0700

    add admin button to header

--- public/js/services/global.js
+++ public/js/services/global.js
@@ -4,7 +4,8 @@ angular.module('lrs').factory("Global", [
     var _this = this;
     _this._data = {
       user: window.user,
-      authenticated: !! window.user
+      authenticated: !! window.user,
+      isAdmin: (window.user && window.user.role === 'admin')
     };
 
     return _this._data;
