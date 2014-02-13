89086047731270f87cd8a762160393c96f05b8ce
Seth Howard <smiles.seth@gmail.com>
2014-02-12 17:32:45 -0700

    updated

--- public/js/controllers/reserve.js
+++ public/js/controllers/reserve.js
@@ -1,7 +1,23 @@
 angular.module('lrs').controller('ReserveController', ['$scope', 'Global', 'Restangular', function ($scope, Global, Restangular) {
   $scope.global = Global;
 
-  $scope.d3Data = Restangular.all("admin/api/Computers").getList().$object;
+
+  // Display all entries in database
+  $scope.d3Data = function(model) {
+    $http.get('/api/computers').success(function(data) {
+      // $scope.modelTitle = model;
+
+      // var headers = [];
+
+      // for(var header in data.data[0]) {
+      //   headers.push(header);
+      // }
+
+      // $scope.currentModel = model;
+      // $scope.modelHeaders = headers;
+      // $scope.modelData = data.data;
+    });
+  };
 
   $scope.d3OnClick = function(item) {
         $scope.stationData = item;
