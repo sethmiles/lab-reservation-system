75a5dd759dbc493c9feef2716407f5b037841888
Seth Howard <smiles.seth@gmail.com>
2014-02-12 17:42:43 -0700

    removed restangular

--- public/js/app.js
+++ public/js/app.js
@@ -1,42 +1,28 @@
-angular.module('lrs', ['ngRoute', 'ui.bootstrap', 'restangular']);
+angular.module('lrs', ['ngRoute', 'ui.bootstrap']);
 
 //Setting up route
-angular.module('lrs').config(['$routeProvider', '$locationProvider', 'RestangularProvider',
-  function($routeProvider, $locationProvider, RestangularProvider) {
-    $routeProvider.when('/', {
-      templateUrl: 'views/index.html',
-    });
-    $routeProvider.when('/reserve', {
-      templateUrl: 'views/reserve.html',
-      controller: 'ReserveController'
-    });
-    $routeProvider.when('/calendar', {
-      templateUrl: 'views/calendar.html',
-    });
-    $routeProvider.when('/policies', {
-      templateUrl: 'views/policies.html',
-    });
-    $routeProvider.when('/admin', {
-      templateUrl: 'views/admin.html',
-      controller: 'AdminController'
-    });
+angular.module('lrs').config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $routeProvider.when('/', {
+          templateUrl: 'views/index.html',
+        });
+        $routeProvider.when('/reserve', {
+          templateUrl: 'views/reserve.html',
+          controller: 'ReserveController'
+        });
+        $routeProvider.when('/calendar', {
+          templateUrl: 'views/calendar.html',
+        });
+        $routeProvider.when('/policies', {
+          templateUrl: 'views/policies.html',
+        });
+        $routeProvider.when('/admin', {
+          templateUrl: 'views/admin.html',
+          controller: 'AdminController'
+        });
 
-    $locationProvider.html5Mode(true);
+        $locationProvider.html5Mode(true);
 
-    RestangularProvider.setBaseUrl('localhost:3000');
-      // RestangularProvider.setDefaultRequestParams({ apiKey: '4f847ad3e4b08a2eed5f3b54' });
-      // RestangularProvider.setRestangularFields({
-      //   id: '_id.$oid'
-      // });
-      
-      RestangularProvider.setRequestInterceptor(function(elem, operation, what) {
-        
-        if (operation === 'put') {
-          elem._id = undefined;
-          return elem;
-        }
-        return elem;
-      });
 
 
     }
--- public/js/controllers/reserve.js
+++ public/js/controllers/reserve.js
@@ -1,33 +1,22 @@
-angular.module('lrs').controller('ReserveController', ['$scope', 'Global', 'Restangular', function ($scope, Global, Restangular) {
+angular.module('lrs').controller('ReserveController', ['$scope', 'Global', '$http', function ($scope, Global, $http) {
   $scope.global = Global;
 
 
   // Display all entries in database
-  $scope.d3Data = function(model) {
+   
     $http.get('/api/computers').success(function(data) {
-      // $scope.modelTitle = model;
-
-      // var headers = [];
-
-      // for(var header in data.data[0]) {
-      //   headers.push(header);
-      // }
-
-      // $scope.currentModel = model;
-      // $scope.modelHeaders = headers;
-      // $scope.modelData = data.data;
+      $scope.d3Data = data.data;
     });
-  };
 
-  $scope.d3OnClick = function(item) {
+    $scope.d3OnClick = function(item) {
         $scope.stationData = item;
         $scope.$apply();
         $scope.stationData.events = [
             {start:800, end: 900},
             {start: 1400, end: 1600}
         ];
-  };
+    };
 
-  $scope.today = new Date();
+    $scope.today = new Date();
 
 }]);
